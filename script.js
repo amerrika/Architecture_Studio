3b98ea4c8daab3d232163d940919bc27847adeb3
Amerrika <amer.tricic@live.com>
2023-06-21 14:19:10 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const btnToggle = document.querySelector(".btn--toggle");
-console.log(btnToggle)
+const mainNav = document.querySelector(".main-nav");
 
-btnToggle.addEventListener("click", () => {
-    console.log(document.querySelector(".main-nav-mobile"))
-    document.querySelector(".main-nav-mobile").classList.toggle("display-none");
-})
+btnToggle.addEventListener("click", function () {
+  // console.log(links.classList);
+  // console.log(links.classList.contains("random"));
+  // console.log(links.classList.contains("links"));
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links");
+  // } else {
+  //   links.classList.add("show-links");
+  // }
+  mainNav.classList.toggle("show-links");
+});
