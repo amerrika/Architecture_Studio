56a9de64e4780c2b1a97f75345ce8732146be279
Amerrika <amer.tricic@live.com>
2023-07-01 14:33:15 +0200

    update

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ btnToggle.addEventListener("click", function () {
   // }
   mainNav.classList.toggle("show-links");
 });
+
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 10,
+  center: new google.maps.LatLng(59.91916, 30.32910),
+  mapTypeId: "roadmap"
+});
